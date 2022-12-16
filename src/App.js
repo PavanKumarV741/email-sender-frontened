f6315fe8f539cc87ab67fd30ebb5c3432c79fde1
PavanKumarV741 <102721175+PavanKumarV741@users.noreply.github.com>
2022-12-16 20:49:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './component/Header';
+import Home from './component/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
